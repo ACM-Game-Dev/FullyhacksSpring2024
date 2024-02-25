7407f34facfc1ffdd0ee5f675bab09014ae31b11
Jordan Muir <jordanhmuir@gmail.com>
2024-02-24 20:00:25 -0800

    Spire timer working, and spire detection fixed (currently)

--- FullyHacks/Spire2D.gd
+++ FullyHacks/Spire2D.gd
@@ -2,6 +2,13 @@ extends Area2D
 
 signal prox
 
+const MAX_CHARGE = 100
+
+var charging = false
+
+var charge = 0
+var player = null
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -12,5 +19,18 @@ func _process(delta):
 	pass
 
 func _on_body_entered(body):
+	#if body == player:
+	charging = true
 	prox.emit()
 	print("Player Entered!") # Replace with function body.
+
+
+func _on_charge_timer_timeout():
+	if charging:
+		charge += 0.1
+		print(charge)
+
+
+func _on_body_exited(body):
+	charging = false
+	pass # Replace with function body.
