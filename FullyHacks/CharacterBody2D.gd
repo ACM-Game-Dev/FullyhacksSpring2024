b991477abd61542938a61cfc630abaaedb5e188a
leauxgan1 <logan.langdon@yahoo.com>
2024-02-24 18:38:33 -0800

    Initial version of movement, map, player character

---
+++ FullyHacks/CharacterBody2D.gd
@@ -0,0 +1,29 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var prevVelocity = Vector2.ZERO
+
+
+func _physics_process(delta):
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction_x = Input.get_axis("ui_left", "ui_right")
+	var direction_y = Input.get_axis("ui_up", "ui_down")
+	if direction_x or direction_y:
+		velocity.x = direction_x * SPEED
+		velocity.y = direction_y * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.y = move_toward(velocity.y, 0 , SPEED)
+	
+	velocity.x = lerp(prevVelocity.x,velocity.x, 0.1)
+	velocity.y = lerp(prevVelocity.y,velocity.y, 0.1)
+	prevVelocity = velocity
+
+	move_and_slide()
