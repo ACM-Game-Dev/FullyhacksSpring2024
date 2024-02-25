9f01733fa7be4f9e1af3da7dd597516c32cafe79
leauxgan1 <logan.langdon@yahoo.com>
2024-02-24 22:26:04 -0800

    Added rudimentary spawn system and updated spire charge logic

--- FullyHacks/Spire2D.gd
+++ FullyHacks/Spire2D.gd
@@ -3,11 +3,14 @@ extends Area2D
 signal prox
 
 const MAX_CHARGE = 100
+const CHARGE_RATE = 1
+const CHARGE_FALLOFF_RATE = 0.5
 
 var charging = false
 
 var charge = 0
 var player = null
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -16,7 +19,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	charge = clamp(charge, 0, 10)
+	charge = clamp(charge, 0, MAX_CHARGE)
 
 func _on_body_entered(body):
 	if body.has_method("_player"):
@@ -26,11 +29,11 @@ func _on_body_entered(body):
 
 
 func _on_charge_timer_timeout():
-	if charging:
-		charge += 0.1
+	if charging and charge < MAX_CHARGE:
+		charge += CHARGE_RATE
 		print(charge)
-	if not charging:
-		charge -= 0.05
+	if not charging and charge > 0:
+		charge -= CHARGE_FALLOFF_RATE
 
 
 
